b738825bdc06072f43955f201de2cf55615edba9
Zainabfarah <zainabfarah890@gmail.com>
2024-01-23 15:09:20 +0100

    .

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const helmet = require('helmet');
 const dataRoutes = require('./routes/data_routes');
 
 const app = express();
+app.use(helmet());
 app.use(cors());  // Use CORS middleware without any restrictions
 app.use(bodyParser.json());
 app.use('/data', dataRoutes);
