94a9dafecf69a153c2a14cb46d332df0dfcea879
JonasB91 <jonas.backas@hotmail.com>
2024-01-24 12:32:09 +0100

    Authentisering och Generera skyddad data beroende på Token och roll använder ramverk axios

--- src/routes/data_routes.js
+++ src/routes/data_routes.js
@@ -1,10 +1,39 @@
 
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const axios = require('axios');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.use(async (req, res, next) => {
+  try {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split('')[1];
+        if(!token) return res.status(401).send('Access Denied');
+
+        const { data } = await axios.get('http://auth-server/auth/verify', {
+          headers: {
+            authorization: `Bearer ${token}`,
+            },
+        });
+
+        req.user = data;
+        next();
+  } catch (error) {
+    res.status(401).send('Invalid Token');
+  }
+});
+
+router.get('/secret-data', (req, res) => {
+    if(req.user.role === 'admin') {
+      res.json({ data: 'Secret data for admin!' });
+    } else {
+      res.json({ data: 'Secret data for user!' });
+    }
+});
+
+
+/* router.get('/', (req, res) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
@@ -20,7 +49,7 @@ router.get('/', (req, res) => {
   } catch {
     res.status(401).send('Invalid Token');
   }
-});
+}); */
 
 module.exports = router;
 
