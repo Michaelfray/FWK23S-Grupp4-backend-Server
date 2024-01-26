6b368df890ad82a0f7df50ab116ac5dcc689a7f5
JonasB91 <jonas.backas@hotmail.com>
2024-01-26 12:33:49 +0100

    Lägger upp senaste versionen av backend la till lite comments

--- src/routes/data_routes.js
+++ src/routes/data_routes.js
@@ -5,10 +5,12 @@ const fs = require('fs');
 env.config();
 
 const router = express.Router();
-const userDataPath = `${__dirname}/../user.json`;
-const userData = JSON.parse(fs.readFileSync(userDataPath, 'utf-8'));
-const SECRET_KEY = process.env.SECRET_KEY || 'yourFallbackSecretKey'; // use process.env.SECRET_KEY if available, fallback to a default key
+const userDataPath = `${__dirname}/../user.json`; // går in i rätt mapp user.json
+const userData = JSON.parse(fs.readFileSync(userDataPath, 'utf-8')); // läser av user.json via path ovanför
+const SECRET_KEY = process.env.SECRET_KEY || 'yourFallbackSecretKey'; // använder SECRET_KEY som lagras i env annars en default key att luta sig mot.
 
+//get-request för att verifiera rätt användare som sedan kollar token och du får tillgång till din role för anpassad data.
+//fetchas från frontend till backend när du är inloggad för att fetcha data beroende på role.
 router.get('/', (req, res) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
@@ -18,19 +20,20 @@ router.get('/', (req, res) => {
   try {
     const verified = jwt.verify(token, SECRET_KEY);
 
-    // Fetch user information from user.json based on the userId or username in the token
+    // Fetcha users beroende på userID och username
     const user = userData.users.find(u => u.id === verified.userId || u.username === verified.username);
 
+    //Om ingen användare skicka felkod 401, med invalid token
     if (!user) {
       return res.status(401).send('Invalid Token');
     }
-
-    // Now you can access the user's role and customize your response
+    // access user role och anpassa info för vilken användares response.
     if (user.role === 'admin') {
       res.json({ data: 'Secret data for admin!' });
     } else {
       res.json({ data: 'Secret data for user!' });
     }
+    
   } catch (error) {
     console.error('Token verification error:', error.message);
     res.status(401).send('Invalid Token');
