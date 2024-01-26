60044701cd057021579243ad7f9d1f9f1ff06136
JonasB91 <jonas.backas@hotmail.com>
2024-01-26 12:50:33 +0100

    Lägger till senaste version med cookies imported osv

--- src/routes/data_routes.js
+++ src/routes/data_routes.js
@@ -9,6 +9,12 @@ const userDataPath = `${__dirname}/../user.json`; // går in i rätt mapp user.j
 const userData = JSON.parse(fs.readFileSync(userDataPath, 'utf-8')); // läser av user.json via path ovanför
 const SECRET_KEY = process.env.SECRET_KEY || 'yourFallbackSecretKey'; // använder SECRET_KEY som lagras i env annars en default key att luta sig mot.
 
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');  // Replace with your frontend origin
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 //get-request för att verifiera rätt användare som sedan kollar token och du får tillgång till din role för anpassad data.
 //fetchas från frontend till backend när du är inloggad för att fetcha data beroende på role.
 router.get('/', (req, res) => {
@@ -33,7 +39,7 @@ router.get('/', (req, res) => {
     } else {
       res.json({ data: 'Secret data for user!' });
     }
-    
+
   } catch (error) {
     console.error('Token verification error:', error.message);
     res.status(401).send('Invalid Token');
--- src/server.js
+++ src/server.js
@@ -4,12 +4,17 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const dataRoutes = require('./routes/data_routes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(helmet());
-app.use(cors());  // Use CORS middleware without any restrictions
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })); 
 app.use(bodyParser.json());
 app.use('/data', dataRoutes);
+app.use(cookieParser());;
 
 module.exports = app;
 
